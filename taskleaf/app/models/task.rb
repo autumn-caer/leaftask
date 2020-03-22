7c0c6e27530f44b47c7044b1f30bc5b0ae631221
root <root@LAPTOP-D6RSN1GL.localdomain>
2020-03-22 11:17:01 +0900

    select_modify_completed

--- taskleaf/app/models/task.rb
+++ taskleaf/app/models/task.rb
@@ -3,6 +3,9 @@ class Task < ApplicationRecord
     validates :name, length: { maximum: 30}
     validate :validate_name_not_including_comma
     before_validation :set_nameless_name
+    belongs_to :user
+
+    scope :recent, -> {order(created_at: :desc)}
 
     private
 
